d8099e8abb637ca1c849b2e5a1d0991d1839d075
Shupilkin Stanislav Yurevich <stanislav.shupilkin@mac-01099.office.lamoda.ru>
2019-11-09 11:23:37 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+OUT_FORMAT=${OUT_FORMAT:-json}
 cd "$GITHUB_WORKSPACE"
+ls -alt
+go env
 
 if [ ! -z "${INPUT_CONFIG}" ]; then CONFIG="--config $INPUT_CONFIG"; fi
 
 if [ ! -z "${GITHUB_TOKEN}" ];
 then
-  sh -c "GO111MODULE=on golangci-lint run $CONFIG --out-format json | golangci-lint-action"
+  sh -c "GO111MODULE=on golangci-lint run $CONFIG --out-format ${OUT_FORMAT} | golangci-lint-action"
 else
   echo "Annotations inactive. No GitHub token provided"
   sh -c "GO111MODULE=on golangci-lint run $CONFIG"
